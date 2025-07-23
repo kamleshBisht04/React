7cbf812ca95c084e5dbe29dea9346bc4d1ac0b50
kamleshBisht04 <kamleshbisht04@gmail.com>
2025-07-23 19:01:22 +0530

    make a single component for steps

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -13,6 +13,16 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -79,3 +89,99 @@ export default function App() {
     </>
   );
 }
+
+
+
+//  2 version 
+
+// import { useState } from "react";
+
+// const messages = [
+//   "Learn React ⚛️",
+//   "Apply for jobs 💼",
+//   "Invest your new income 🤑",
+//   "Happy Employee and family😜",
+//   "Working hard agian😜",
+//   "Get Bonous ❤️",
+//   "Gotta permotion😜",
+//   "New Joining and perfect life😁",
+//   "Working hard for company 💢",
+// ];
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Steps />
+//       <Steps />
+//     </div>
+//   );
+// }
+
+
+// function Steps() {
+//   const [step, setStep] = useState(1);
+//   const [isOpen, setIsOpen] = useState(true);
+
+//   const totalStep = messages.length;
+//   const stepArray = Array.from({ length: totalStep }, (_, i) => i + 1);
+
+//   function handleNext() {
+//     if (step < totalStep) setStep(step + 1);
+//   }
+
+//   function handlePevious() {
+//     if (step > 1) setStep(step - 1);
+//   }
+
+//   function handleStep(num) {
+//     setStep(num);
+//   }
+
+//   return (
+//     <div>
+//       <button className="close" onClick={() => setIsOpen(!isOpen)}>
+//         &times;
+//       </button>
+//       <div>
+//         {isOpen && (
+//           <div className="steps">
+//             <div className="numbers">
+//               {stepArray.map((num) => (
+//                 <div
+//                   key={num}
+//                   className={step === num ? "active" : ""}
+//                   onClick={() => handleStep(num)}
+//                 >
+//                   {num}
+//                 </div>
+//               ))}
+
+//               {/* <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+//         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+//         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+//         <div className={`${step >= 4 ? "active" : ""}`}>4</div> */}
+//             </div>
+
+//             <p className="message">
+//               Step {step} : {messages[step - 1]}
+//             </p>
+//             <div className="buttons">
+//               <button
+//                 style={{ backgroundColor: "#7950f2", color: "#fff" }}
+//                 onClick={handlePevious}
+//               >
+//                 Previous
+//               </button>
+//               <button
+//                 style={{ backgroundColor: "#7950f2", color: "#fff" }}
+//                 onClick={handleNext}
+//               >
+//                 Next
+//               </button>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
