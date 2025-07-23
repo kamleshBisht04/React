6ce9c14f9353e835a47a1e9ea8549369bfd77ee6
kamleshBisht04 <kamleshbisht04@gmail.com>
2025-07-23 11:46:58 +0530

    handling the events and steps project complete

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+  "Happy Employee and family😜",
+  "Working hard agian😜",
+  "Get Bonous ❤️",
+  "Gotta permotion😜",
+  "New Joining and perfect life😁",
+  "Working hard for company 💢",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  const totalStep = messages.length;
+  const stepArray = Array.from({ length: totalStep }, (_, i) => i + 1);
+
+  function handleNext() {
+    if (step < totalStep) setStep(step + 1);
+  }
+
+  function handlePevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        {stepArray.map((num) => (
+          <div key={num} className={step === num ? "active" : ""}>
+            {num}
+          </div>
+        ))}
+
+        {/* <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={`${step >= 4 ? "active" : ""}`}>4</div> */}
+      </div>
+
+      <p className="message">
+        Step {step} : {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePevious}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
