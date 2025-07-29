09a4e5806c43924f1a6562f17f9600a132adbe78
kamleshBisht04 <kamleshbisht04@gmail.com>
2025-07-29 09:38:04 +0530

    adding the stats render

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -36,7 +36,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggle={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -139,10 +139,24 @@ function Items({ items, onDeleteItem, onToggle }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (items.length)
+    return (
+      <p className="stats">
+        <em> 🔷 Start adding some items to your packing list.✈️ </em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
-    <div className="stats">
-      <em>💼 You have X items on your list,and you already packed X (x%)</em>
-    </div>
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
   );
 }
