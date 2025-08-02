26feeb6dfa72830150cc46e931cbfcfd2172c24e
kamleshBisht04 <kamleshbisht04@gmail.com>
2025-08-02 20:59:31 +0530

    tip calculator

--- 06--tipCalcuator/src/App.js
+++ 06--tipCalcuator/src/App.js
@@ -21,50 +21,41 @@ function TipCalculator() {
     setPercentage1(0);
     setPercentage2(0);
   }
-
   return (
     <div className="form_grid">
-      <BillInput bill={bill} onSetBill={setBill} />
+      <BillInput bill={bill} onSelect={setBill} />
+
       <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
         How did you like the service?
       </SelectPercentage>
       <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
         How did your friend like the service?
       </SelectPercentage>
-
-      {bill > 0 && (
-        <>
-          <Output bill={bill} tips={tips} />
-          <Reset onReset={handleReset} />
-        </>
-      )}
+      <Output bill={bill} tips={tips} />
+      <Reset onReset={handleReset} />
     </div>
   );
 }
 
-function BillInput({ bill, onSetBill }) {
+function BillInput({ bill, onSelect }) {
   return (
     <>
       <label>How much was the bill?</label>
       <input
         type="text"
-        value={bill}
         placeholder="Enter the bill amount ?"
-        onChange={(e) => onSetBill(+e.target.value)}
+        value={bill}
+        onChange={(e) => onSelect(Number(e.target.value))}
       />
     </>
   );
 }
 
-function SelectPercentage({ percentage, onSelect, children }) {
+function SelectPercentage({ children, percentage, onSelect }) {
   return (
     <>
       <label>{children}</label>
-
-      <select
-        value={percentage}
-        onChange={(e) => onSelect(Number(e.target.value))}
-      >
+      <select value={percentage} onChange={(e) => onSelect(+e.target.value)}>
         <option value="0">Dissatisfied (0%)</option>
         <option value="5">It was okay (5%)</option>
         <option value="10">It was good (10%)</option>
@@ -74,17 +65,21 @@ function SelectPercentage({ percentage, onSelect, children }) {
   );
 }
 
-function Output({ bill, tips }) {
+function Output({ tips, bill }) {
   return (
     <>
-      <label>Total Payable:</label>
+      <label>Total Payable :</label>
       <h3>
-        You pay {bill + tips}₹ ({bill}₹+ {tips}₹tip)
+        You pay {bill + tips}₹ (${bill} ₹ + {tips}₹ tip)
       </h3>
     </>
   );
 }
 
 function Reset({ onReset }) {
-  return <button onClick={onReset}>Reset</button>;
+  return (
+    <div>
+      <button onClick={onReset}>Reset</button>
+    </div>
+  );
 }
