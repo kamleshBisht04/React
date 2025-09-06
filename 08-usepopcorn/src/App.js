fbf6973c1fff60af8a6aa9c1c8051af1a92da9d6
kamleshBisht04 <kamleshbisht04@gmail.com>
2025-09-06 10:10:50 +0530

    design the nav bar

--- 08-usepopcorn/src/App.js
+++ 08-usepopcorn/src/App.js
@@ -45,4 +45,39 @@ const tempWatchedData = [
   },
 ];
 
+export default function App() {
+  return <Navbar />;
+}
 
+function Navbar() {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResult />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <input className="search" type="text" placeholder="Search Movies..." />
+  );
+}
+
+function NumResult() {
+  return (
+    <p className="num-results">
+      Found<strong>X</strong> results
+    </p>
+  );
+}
