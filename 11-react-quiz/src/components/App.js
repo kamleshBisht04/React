7568370e68bbf75830c7912f61f2247ee70038ed
kamleshBisht04 <kamleshbisht04@gmail.com>
2025-11-15 17:26:46 +0530

    Adding progress bar and setting the score acording to question

--- 11-react-quiz/src/components/App.js
+++ 11-react-quiz/src/components/App.js
@@ -5,6 +5,8 @@ import StartScreen from "./StartScreen";
 import Error from "./Error";
 import Loader from "./Loader";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -31,14 +33,22 @@ function reducer(state, action) {
         points:
           action.payload === question.correctOption ? state.points + question.points : state.points,
       };
+
+    case "nextQuestion":
+      return { ...state, index: state.index++, answer: null };
+
     default:
       throw new Error("Unkown action");
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   const numQuestion = questions.length;
+  const maxPossiablePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
 
   useEffect(function () {
     fetch("http://localhost:9000/questions")
@@ -55,7 +65,17 @@ function App() {
         {status === "error" && <Error />}
         {status === "ready" && <StartScreen numQuestion={numQuestion} dispatch={dispatch} />}
         {status === "active" && (
-          <Question question={questions[index]} dispatch={dispatch} answer={answer} />
+          <>
+            <Progress
+              numQuestion ={numQuestion}
+              index={index}
+              points={points}
+              maxPossiablePoints={maxPossiablePoints}
+              answer= {answer}
+            />
+            <Question question={questions[index]} dispatch={dispatch} answer={answer} />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
